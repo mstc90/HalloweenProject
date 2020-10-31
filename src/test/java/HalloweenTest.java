298fe434e8d84843442b39b3bbfea2fe83d788c2
mstc90 <Kalendar@2020>
2020-10-31 19:11:50 -0400

    no treat or trick commit

--- src/test/java/HalloweenTest.java
+++ src/test/java/HalloweenTest.java
@@ -1,6 +1,10 @@
 public class HalloweenTest {
 
     // I am about to give everyone candy for halloween
+    // are we there yet ???? Hello Hello
+    // lets play a the game
+    // its fun to drive so far
 
-
+    // there will be no treat and trick this year
+    // stay home and be safe and wear your mask outside
 }
